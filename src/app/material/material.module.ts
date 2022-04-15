14a1bcd7918ed96e2dcfdbab17be8bdc5801af4e
Ken Mbesa <kenmbesa@gmail.com>
2022-04-15 10:54:57 +0300

    Added content to the Skills and Projex components.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,13 +4,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 const materials = [
   MatSidenavModule,
   MatFormFieldModule,
   MatListModule,
   MatIconModule,
-  MatDialogModule
+  MatDialogModule,
+  MatProgressBarModule
 ]
 
 @NgModule({
