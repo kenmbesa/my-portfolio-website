f9b419b7041f09304675d0f549f7214582401a53
Ken Mbesa <kenmbesa@gmail.com>
2022-04-22 05:27:51 +0300

    Styled the three My-Business components with "Under Construction" text.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { HelabagComponent } from './components/my-businesses/helabag/helabag.component';
+import { JedwaliComponent } from './components/my-businesses/jedwali/jedwali.component';
+import { PabzzyComponent } from './components/my-businesses/pabzzy/pabzzy.component';
 import { MyProjectsComponent } from './components/my-projects/my-projects.component';
 import { MySkillsComponent } from './components/my-skills/my-skills.component';
 
@@ -14,6 +17,9 @@ const routes: Routes = [
   { path: 'my-projects', component: MyProjectsComponent},
   { path: 'contact-me', component: ContactMeComponent},
   { path: 'blog', component: BlogComponent},
+  { path: 'pabzzy', component: PabzzyComponent},
+  { path: 'helabag', component: HelabagComponent},
+  { path: 'jedwali', component: JedwaliComponent}
 ];
 
 @NgModule({
